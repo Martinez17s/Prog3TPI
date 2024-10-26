811d3b10577d54146865d22683fb8c45c5dd53ab
Lorenzo Martinez <lmar190504@gmail.com>
2024-10-25 22:22:20 -0300

    Add Interfaces

--- Application/Interfaces/EnrollmentService.cs
+++ Application/Interfaces/EnrollmentService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Application.DTOs.Requests;
+using Application.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace Application.Interfaces
 {
-    internal class EnrollmentService
+    public interface IEnrollmentService
     {
+        Task<EnrollmentDto> CreateAsync(EnrollmentCreateRequest request);
+
+        Task<EnrollmentDto> GetEnrollmentAsync(int id);
+
+        Task<List<EnrollmentDto>> GetAllAsync();
+
+        Task<EnrollmentDto> UpdateAsync(EnrollmentDto enrollment, int id);
+
+        Task DeleteAsync(int id);
     }
 }
--- Application/Interfaces/IClientService.cs
+++ Application/Interfaces/IClientService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Application.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Application.Interfaces
 {
-    internal class Class4
+    public interface IClientService
     {
+        Task<List<SubjectDto>> GetClientSubjects(int clientId);
     }
 }
