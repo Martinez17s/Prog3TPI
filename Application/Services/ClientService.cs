fb4b340c290157bd0e889060d9a30859c1144662
Lorenzo Martinez <lmar190504@gmail.com>
2024-10-25 21:58:08 -0300

    Add Services Client and Enrollment

---
+++ Application/Services/ClientService.cs
@@ -0,0 +1,35 @@
+﻿using Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class ClientService : IClientService
+    {
+        private readonly IClientRepository _clientService;
+        public ClientService(IClientRepository clientService)
+        {
+            _clientService = clientService;
+        }
+
+        public async Task<List<SubjectDto>> GetClientSubjects(int clientId)
+        {
+            var subjects = _clientService.GetClientSubjects(clientId);
+            var subjectsDto = new List<SubjectDto>();
+            foreach (var subject in subjectsDto)
+            {
+                var activityDto = new SubjectDto()
+                {
+                    Title = subject.Title,
+                    Description = subject.Description,
+                    ProfessorId = subject.ProfessorId,
+                };
+                subjectsDto.Add(activityDto);
+            }
+            return subjectsDto;
+        }
+    }
+}
