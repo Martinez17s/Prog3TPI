23191c8f72f9bdefda669ba75096b0e655c0e780
Ramiro Trevisan <ramirotrevisan@gmail.com>
2024-10-25 22:23:03 -0300

    Repositories

---
+++ Infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class ClientRepository : EfRepository<Client>, IClientRepository
+    {
+        public ClientRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public ICollection<Subject> GetClientSubjects(int clientId)
+        {
+            var client = _appDbContext.Clients
+                        .Include(c => c.Enrollments)
+                        .ThenInclude(a => a.Subject)
+                        .SingleOrDefault(c => c.Id == clientId);
+
+            if (client == null)
+                throw new KeyNotFoundException($"Client with ID {clientId} not found.");
+            return client.Enrollments.Select(e => e.Subject).ToList();
+        }
+
+    }
+}
---
+++ Infrastructure/Repositories/ProfessorRepository.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class ProfessorRepository : EfRepository<Professor>, IProfessorRepository
+    {
+        public ProfessorRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public ICollection<Client> GetClientsEnrolledInMySubjects(int professorId)
+        {
+            var professor = _appDbContext.Professors
+                .Include(p => p.Subjects)
+                .ThenInclude(a => a.Enrollments)
+                .ThenInclude(e => e.Client)
+                .SingleOrDefault(p => p.Id == professorId);
+
+            if (professor == null)
+            {
+                throw new KeyNotFoundException($"Professor with ID {professorId} not found.");
+            }
+
+            return professor.Subjects.SelectMany(a => a.Enrollments.Select(e => e.Client)).ToList();
+        }
+    }
+}
