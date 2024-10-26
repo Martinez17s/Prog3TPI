1ffb5e34990badf0d9d5bab08c639701784b809c
hsz <santinogz131@gmail.com>
2024-10-25 22:01:39 -0300

    code

--- Prog3TPI/Controllers/AuthenticationController.cs
+++ Prog3TPI/Controllers/AuthenticationController.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Application.DTOs.Requests;
+using Application.Interfaces;
+using Domain.Exceptions;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Application.DTOs
+namespace ProyectoP3.Api.Controllers
 {
-    internal class AuthenticationController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+        private readonly ICustomAuthentication _customAuthentication;
+
+        public AuthenticationController(IConfiguration configuration, ICustomAuthentication customAuthentication)
+        {
+            _configuration = configuration; // Para usar appsetting.json
+            _customAuthentication = customAuthentication;
+        }
+
+        [HttpPost("Authenticate")]
+        public async Task<ActionResult<string>> Authenticate([FromBody] AuthenticationRequest authenticationRequest)
+        {
+            try
+            {
+                string token = await _customAuthentication.AutenticarAsync(authenticationRequest);
+                return Ok(token);
+            }
+            catch (NotAllowedException ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+
+        }
     }
 }
--- Prog3TPI/Controllers/ClientController.cs
+++ Prog3TPI/Controllers/ClientController.cs
@@ -1,6 +1,41 @@
-﻿namespace Prog3TPI.Controllers
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P3TPI.Controllers
 {
-    public class ClientController
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Policy = "ClientPolicy")]
+    public class ClientController : ControllerBase
     {
+        private readonly IClientService _clientService;
+        public ClientController(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
+
+        [HttpGet("{clientId}/GetAllSubjectsEnrollments")]
+        public async Task<ActionResult<List<SubjectDto>>> GetAllSubjectsEnrollments([FromRoute] int clientId)
+        {
+            try
+            {
+                var subjects = await _clientService.GetClientSubjects(clientId);
+                if (subjects == null || !subjects.Any())
+                {
+                    return NotFound($"No subjects found for client with ID {clientId}.");
+                }
+                return Ok(subjects);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
     }
 }
